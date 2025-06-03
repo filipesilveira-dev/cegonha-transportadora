861f50b47ae8b91d2d9e606a4a1c2a1268b0fb74
Filipe <filipesilveira.dev@gmail.com>
2025-06-03 20:42:46 -0300

    funcionalidades ok

--- script.js
+++ script.js
@@ -20,3 +20,31 @@ document.getElementById("cep").addEventListener("blur", (evento)=>{ //vai "escut
         })
         .catch(error => console.error("Erro ao busca CEP: ", error));
 })
+
+const meuFormulario = document.getElementById("meuFormulario");
+
+// Salvar dados do formulário
+function salvarForm() {
+    const dados = new FormData(meuFormulario);  //pega todos os dados de "form"
+    const obj = Object.fromEntries(dados);      //pega todas as entradas do formulário e transforma em um objeto só
+    localStorage.setItem('formInfo', JSON.stringify(obj));  //converte um objeto em texto (string) para poder guardar em localStorage (persistência)
+}
+
+// Restaurar dados do formulário
+function restaurarForm() {
+    const dados = localStorage.getItem('formInfo'); //pega o que foi salvo no localStorage
+    if (dados) {                                    //se retornar algum "dado", então...
+        const obj = JSON.parse(dados);              //converta o jason em objeto
+        for (let [name, value] of Object.entries(obj)) {    //para cada campo salvo, será preenchido com o valor. Ex: name="nome", valor="Filipe"
+            const campo = meuFormulario.querySelector(`[name="${name}"]`);  //procure o campo com o name = "name" (importância do name ser igual ao campo do nome?)
+            if (campo) campo.value = value; //vai retornar o valor do campo. Se campo for igual a "nome", vai retornar o valor do campo "nome" ("Filipe", no caso)
+              
+        }
+    }
+}
+
+// Salva quando o usuário digita
+meuFormulario.addEventListener('input', salvarForm);
+
+// Restaura quando a página carrega
+document.addEventListener('DOMContentLoaded', restaurarForm);
